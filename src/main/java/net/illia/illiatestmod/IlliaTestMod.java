f1e0a77c9217947ef21e26c784289eeb51ed0904
Illia Zhdanov <il.25.zhd@gmail.com>
2022-08-13 17:14:24 +0300

    Added A Fucking Ruby

--- src/main/java/net/illia/illiatestmod/IlliaTestMod.java
+++ src/main/java/net/illia/illiatestmod/IlliaTestMod.java
@@ -1,22 +1,27 @@
 package net.illia.illiatestmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.*;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class IlliaTestMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "illiatestmod"; 
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "illiatestmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(IlliaTestMod.MOD_ID);
+	public static final ItemGroup ILLIA_TEST_MOD = FabricItemGroupBuilder.build(new Identifier(IlliaTestMod.MOD_ID, "illiatestmod"), () -> new ItemStack(Items.BARRIER));
+	public static final Item RUBY = new Item(new FabricItemSettings().group(IlliaTestMod.ILLIA_TEST_MOD));
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
+		Registry.register(Registry.ITEM, new Identifier(IlliaTestMod.MOD_ID, "ruby"), RUBY);
 		LOGGER.info("Hello Fabric world!");
 	}
 }
