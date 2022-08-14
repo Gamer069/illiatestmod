0727cf471323155f8e27937bc182afe71d5c1021
Illia Zhdanov <il.25.zhd@gmail.com>
2022-08-14 13:52:55 +0300

    WTF

--- src/main/java/net/illia/illiatestmod/IlliaTestMod.java
+++ src/main/java/net/illia/illiatestmod/IlliaTestMod.java
@@ -36,7 +36,7 @@ public class IlliaTestMod implements ModInitializer {
 		STILL_MELTED_RUBY = Registry.register(Registry.FLUID, new Identifier(IlliaTestMod.MOD_ID, "melted_ruby"), new MeltedRubyFluid.Still());
 		FLOWING_MELTED_RUBY = Registry.register(Registry.FLUID, new Identifier(IlliaTestMod.MOD_ID, "flowing_melted_ruby"), new MeltedRubyFluid.Flowing());
 		MELTED_RUBY_FLUID_BLOCK = Registry.register(Registry.BLOCK, new Identifier(IlliaTestMod.MOD_ID, "melted_ruby_fluid_block"), new FluidBlock(IlliaTestMod.STILL_MELTED_RUBY, FabricBlockSettings.copyOf(Blocks.WATER)){ });
-		MELTED_RUBY_BUCKET = Registry.register(Registry.ITEM, new Identifier(IlliaTestMod.MOD_ID, "melted_ruby_bucket"), new BucketItem(STILL_MELTED_RUBY, new Item.Settings().recipeRemainder(Items.BUCKET).maxCount(1)));
+		MELTED_RUBY_BUCKET = Registry.register(Registry.ITEM, new Identifier(IlliaTestMod.MOD_ID, "melted_ruby_bucket"), new BucketItem(STILL_MELTED_RUBY, new Item.Settings().recipeRemainder(Items.BUCKET).maxCount(1).group(IlliaTestMod.ILLIA_TEST_MOD)));
 		Registry.register(Registry.BLOCK, new Identifier(IlliaTestMod.MOD_ID, "diamond_chest"), IlliaTestMod.DIAMOND_CHEST);
 		Registry.register(Registry.ITEM, new Identifier(IlliaTestMod.MOD_ID, "diamond_chest"), new BlockItem(IlliaTestMod.DIAMOND_CHEST, new Item.Settings().group(IlliaTestMod.ILLIA_TEST_MOD)));
 		Registry.register(Registry.ITEM, new Identifier(IlliaTestMod.MOD_ID, "ruby"), RUBY);
